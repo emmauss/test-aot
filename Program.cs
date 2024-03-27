b156deb6bc3657b025a35d287f55db6baf4d2a09
Emmanuel Hansen <emmausssss@gmail.com>
2024-03-27 09:14:58 +0000

    arm bugs with timespan

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.ComponentModel.DataAnnotations;
+
+internal class Program
+{
+    static Func<TimeSpan, bool>? Validate { get; set; }
+    private static void Main(string[] args)
+    {
+        // trying to reproduce a different bug encountered on arm but not on arm64 or win_x64, but bug doesn't occur. occurs in live project
+        var timespan = TimeSpan.Zero;
+        Validate = IsValid;
+        Console.WriteLine($"Timespan total ms should be 0, but it's {timespan.TotalMilliseconds}. Comparison timespan.TotalMilliseconds >= 0.0 is not {Validate?.Invoke(timespan) == false}");
+
+        // this crashes on arm, but not arm64 or win_x64
+        var timer = new Timer(_ =>
+        {
+
+        }, null, TimeSpan.FromMilliseconds(60), Timeout.InfiniteTimeSpan);
+        Console.WriteLine("Hello, World!");
+    }
+
+    private static bool IsValid(TimeSpan value) => value.TotalMilliseconds >= 0.0;
+}
